2a28388ab8591e22f7791e3ce59018ae796aff1f
Vaishnavi <87562957+iScreenager@users.noreply.github.com>
2023-04-24 09:09:30 +0530

    Create Assignment-1.cs

---
+++ Assignment-1.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAssignments
+{
+    class Assignments01
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine($"The range of float is {float.MinValue} and {float.MaxValue}");
+            Console.WriteLine($"The range of double is {double.MinValue} and {double.MaxValue}");
+            Console.WriteLine($"The range of decimal is {decimal.MinValue} and {decimal.MaxValue}");
+
+            Console.WriteLine($"The range of double is {double.MinValue} and {double.MaxValue}");
+            Console.WriteLine($"The range of byte is {byte.MinValue} and {byte.MaxValue}");
+            Console.WriteLine($"The range of short is {short.MinValue} and {short.MaxValue}");
+            Console.WriteLine($"The range of long is {long.MinValue} and {long.MaxValue}");
+        }
+    }
+}
